cb105b0716079221f896ac78caa0162417e5a748
Shreya <sshreya1_be24@thapar.edu>
2026-03-11 19:39:55 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+
+API_KEY = "4c4f07c1199e3498477b60ee537e5d6f"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}&units=metric"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    weather = data["weather"][0]["description"]
+    temperature = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    wind = data["wind"]["speed"]
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "°C")
+    print("Humidity:", humidity, "%")
+    print("Wind Speed:", wind, "m/s")
+
+else:
+    print("Error:", response.status_code)
+    print(response.json())
